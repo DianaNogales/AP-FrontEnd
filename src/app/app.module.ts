c7a4d00e489d408571fbfe2305649dbdb6786be7
DianaNogales <diananogales3044@gmail.com>
2023-05-11 03:36:02 -0300

    ZZZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { HomeComponent } from './components/home/home.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    HomeComponent,
+    ExperienciaComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
